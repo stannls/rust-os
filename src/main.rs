68befeefe65af6504f6eaa3728615bd8afffe64f
Yannis Storrer <yannis.storrer@web.de>
2024-01-12 23:09:30 +0100

    Implmented vga_buffer printing

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"HELLOW WORLD";
+
 #[no_mangle] // Don't mangel this functions name
 pub extern "C" fn _start() -> ! {
     // Entry point of the kernel
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
