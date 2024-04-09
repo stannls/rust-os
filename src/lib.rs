6fa1c89a35770712d75359ee3c4112a107b5cb20
Yannis Storrer <yannis.storrer@web.de>
2024-04-09 16:01:20 +0200

    Simple interrupt handling

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,17 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interupts;
+
+pub fn init() {
+    interupts::init_idt();
+}
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -59,6 +65,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
