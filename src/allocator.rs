98ca07cc5cd1daffaa2e999fbde46e06e7670912
Yannis Storrer <yannis.storrer@web.de>
2024-04-28 19:53:51 +0200

    Adjusted  function parameter for new libary version.

--- src/allocator.rs
+++ src/allocator.rs
@@ -29,7 +29,7 @@ pub fn init_heap(mapper: &mut impl Mapper<Size4KiB>, frame_allocator: &mut impl 
         }
     }
     unsafe {
-        ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
+        ALLOCATOR.lock().init(HEAP_START as *mut u8, HEAP_SIZE);
     }
     Ok(())
 }
