6433670c35f012da583c7bed2e7b496af59a9d50
Yannis Storrer <yannis.storrer@web.de>
2024-04-28 19:43:51 +0200

    Used linked list allocator

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,20 @@ entry_point!(kernel_main);
 
 #[no_mangle]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory::BootInfoFrameAllocator;
+    use rust_os::memory::{self, BootInfoFrameAllocator};
+    use rust_os::allocator;
+    use x86_64::VirtAddr;
     println!("Hello World{}", "!");
 
     rust_os::init();
     
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("Heap initialisation failed.");
     let x = Box::new(41);
 
     #[cfg(test)]
